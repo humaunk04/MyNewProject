e223cbb70765f86f1e71ad9b10431d2623abca0a
Humaun Khalique <humaunkhalique@Humauns-MacBook-Pro.local>
2020-01-04 17:41:15 -0500

    adding MyNewProject

--- src/com/humaun/MyTeam.java
+++ src/com/humaun/MyTeam.java
@@ -2,6 +2,52 @@ package com.humaun;
 
 public class MyTeam {
     public static void main(String [] args){
-        System.out.println("mamun");
+        System.out.println("My Team");
+        myFirstJavaMethod();
+        mySecondJavaMethod();
+        myThirdJavaMethod();
+        myFourthJavaMethod();
+        myFifthJavaMethod();
+        mySixJavaMethod();
     }
+    public static void myFirstJavaMethod(){
+
+        int one = 1;
+        String second = "Humaun";
+        int a = 5;
+        int b = 5;
+        int c = 6;
+
+
+        System.out.println(one + a+b+c+ " " +second);
+    }
+    public static void mySecondJavaMethod(){
+
+        int one = 2;
+        String second = "Mamun";
+        System.out.println(one + " " +second);
+    }
+    public static void myThirdJavaMethod(){
+        int one = 3;
+        String second = "Sabbir Bhai";
+        System.out.println(one + " "+ second);
+    }
+    public static void myFourthJavaMethod(){
+        int one = 4;
+        String second = "Masuk Bhai";
+        System.out.println(one + " "+ second);
+    }
+    public static void myFifthJavaMethod(){
+        int one = 5;
+        String second = "Jahid mamu";
+        int add =5;
+        System.out.println(one + add + " "+ second);
+    }
+    public static void mySixJavaMethod(){
+        int one = 6;
+        String second = "Tauhid Mamu";
+        System.out.println(one + " "+second);
+    }
+
+
 }
